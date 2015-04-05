2a44bcef940d4860d6fbc9e5b9fd22f41421f213
krishpop <krishnan.srinivasan@yale.edu>
2015-04-05 18:23:22 -0400

    added pretty-print json

--- sgets.py
+++ sgets.py
@@ -6,13 +6,12 @@ import sys
 import re
 import json
 import requests
-import thread
 
 social_domains = [r'twitter.com/', r'facebook.com/']
 
-google_domain = r'play.google.com/store/apps/details?id='
+google_domain = r'play.google.com/store/apps/details\?id='
 
-apple_pattern = r'itunes.apple.com/app/'
+apple_domain = r'itunes.apple.com/app/'
 
 twitter_pattern = re.compile(
     r'^(?:http)s?://'
@@ -29,13 +28,13 @@ facebook_pattern = re.compile(
 google_pattern = re.compile(
     r'^(?:http)s?://'
     r'(?:www.)?' + google_domain +
-    r'([a-zA-Z0-0_.]+)'
+    r'([a-zA-Z0-9_.]+)'
     )
 
 ios_pattern = re.compile(
     r'^(?:http)s?://'
     r'(?:www.)?' + apple_domain +
-    r'([a-zA-Z0-0_.\-]+)/id'
+    r'([a-zA-Z0-9_.-]+)/id'
     r'([0-9]{9})'
     )
 
@@ -83,19 +82,29 @@ def main():
 
     soup = BeautifulSoup(response.text)
     twitter_match = (None, -1)
+    facebook_match = (None, -1)
+    google_match = (None, -1)
+    ios_match = (None, -1)
     for link in soup.find_all('a'):
         link_url = link.get('href')
         twitter_match = find_id(
             link_url, twitter_pattern, twitter_match, url_domain)
         facebook_match = find_id(
             link_url, facebook_pattern, facebook_match, url_domain)
+        google_match = find_id(
+            link_url, google_pattern, google_match, url_domain)
         ios_match = find_id(link_url, ios_pattern, ios_match, url_domain)
-        google_match = find_id(link_url, google_pattern, google_match,
-                               url_domain)
-    print(twitter_match)
-    print(facebook_match)
-    print(ios_match)
-    print(google_match)
+    json_output = {}
+    if twitter_match[0]:
+        json_output["twitter"] = twitter_match[0]
+    if facebook_match[0]:
+        json_output["facebook"] = facebook_match[0]
+    if ios_match[0]:
+        json_output["ios"] = ios_match[0]
+    if google_match[0]:
+        json_output["google"] = google_match[0]
+    print(json.dumps(json_output, sort_keys=True, indent=4))
+
 
 if __name__ == '__main__':
     main()
